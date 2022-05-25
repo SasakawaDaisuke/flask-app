b5442fabfe37e79207b27a8104e83add42631ae3
SasakawaDaisuke <sasakawa.daisuke.0714@gmail.com>
2022-05-25 16:09:40 +0900

    Flaskの基礎

--- hello.py
+++ hello.py
@@ -1,18 +1,16 @@
 from flask import Flask
+from flask import  render_template
 
 app = Flask(__name__)
 
-html = """
-<h1>サンプルHTML</h1>
-<ul>
-    <li>箇条書き1</li>
-    <li>箇条書き2</li>
-    <li>箇条書き3</li>
-</ul>
-"""
 
+bullets = [
+    "箇条書き1",
+    "箇条書き2",
+    "箇条書き3",
+]
 
-# "/":root のところに表示される
-@app.route("/")
-def hello_world():
-    return html
+# "/" : http://127.0.0.1:5000/ のところに表示
+@app.route("/japan/<city>")
+def hello(city):
+    return render_template("hello.html", city=city, bullets=bullets)  # hello.htmlで使用するcity=URLの引数のcity
